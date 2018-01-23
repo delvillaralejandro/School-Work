05de98c8fe54e558c3778f6a6d2436ed8fedb296
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-23 08:54:37 -0800

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,21 @@
+class Persona{
+  String nombre;
+  int edad;
+  boolean genero;
+  
+  int Caminar(){
+    int posicion;
+    
+    posicion += 1;  
+    return posicion;
+  }
+  
+  String Hablar(){
+    System.out.println("Estoy hablando!");
+    
+  }
+  
+  void Brincar(){}
+  
+  
+}
