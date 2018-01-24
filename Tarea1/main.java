b07c6cc014e9c066e28bcb480bce5f77e75d0eb7
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 12:13:51 -0800

    Update main.java

--- Tarea1/main.java
+++ Tarea1/main.java
@@ -2,11 +2,16 @@ public static void main(String args[]){
   Cocinero chef = new Cocinero();
   Estudiante student = new Estudiante();
   
+  Cocinero chefno = new Cocinero();
+  chefno.Habilidad_Motriz_Poder = new Habilidad_Motriz_NULL();
+  Estudiante estudihambre = new Estudiante();
+  estudihambre.Habilidad_Motriz_Poder = new Habilidad_Motriz_NULL();
+  
   System.out.println("Estas personas puede moverse bien!");
   student.Move();
   chef.Mover();
   
-  Cocinero chefno = new Cocinero();
-  chefno.Habilidad_Motriz
-  Estudiante estudihambre = new Estudiante();
-}
+  System.out.println("Estas personas no tienen POWER!");
+  chefno.Move();
+  estudihambre.Move();
+}//end main
