fa648329b84d564e074a8edfabd91175808b6635
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 11:52:48 -0800

    Create Habilidad_Motriz_Poderoso.java

---
+++ Tarea1/Habilidad_Motriz_Poderoso.java
@@ -0,0 +1,5 @@
+public class Habilidad_Motriz_Poderoso implements Habilidad_Motriz{
+  public void Mover(){
+    System.out.println("Yo puedo caminar y moverme!");
+  }//end Mover
+}//end Habilidad_Motriz_Poderoso
