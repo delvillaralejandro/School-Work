827330ea743b1b76721acfc4f19fbc5fd4efe9c6
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 08:50:30 -0800

    Update Estudiante.java

--- Tarea1/Estudiante.java
+++ Tarea1/Estudiante.java
@@ -1,4 +1,20 @@
 public class Estudiante extends Persona{
   String carrera;
   int matricula;
+  
+    public void Habilidad_Pies(){
+      System.out.println("Puedo usar mis pies!");
+
+      public void Caminar(){
+       System.out.println("Puedo Caminar como Estudiante!"); 
+      }
+
+      public void Brincar(){
+        System.out.pritnln("Puedo brincar como Estudiante");
+  
+    }
+    
+    public void Habilidad_Manos(){
+      System.out.println("Puedo usar mis manos!");
+    }
 }
