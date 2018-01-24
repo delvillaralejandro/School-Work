f45281092c6a684114f8307dd370b4aff9e8dc29
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 09:28:38 -0800

    Update Habilidad_Motriz.java

--- Tarea1/Habilidad_Motriz.java
+++ Tarea1/Habilidad_Motriz.java
@@ -1,5 +1,5 @@
 public interface Habilidad_Motriz{
   public void Habilidad_Motriz_Manos();
-  Public void Habilidad_Motriz_Pies();
+  public void Habilidad_Motriz_Pies();
   public void Habilidad_Motriz_NULL();
 }
