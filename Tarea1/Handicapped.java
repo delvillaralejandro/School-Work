9c8f5011f94bd1230c540bfe69e07971b3608736
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 11:11:34 -0800

    Update Handicapped.java

--- Tarea1/Handicapped.java
+++ Tarea1/Handicapped.java
@@ -1,21 +1,30 @@
 public class Handicapped extends Persona{
-    public void Habilidad_Pies(){
-      System.out.println("No Puedo usar mis pies!");
+  public void Habilidad_Pies(){
+    System.out.println("No Puedo usar mis pies!");
 
-      public void Caminar(){
-       System.out.println("No Puedo Caminar!"); 
-      }
+    public void Caminar(){
+     System.out.println("No Puedo Caminar!"); 
+    }//end Caminar
 
-      public void Brincar(){
-        System.out.pritnln("No Puedo brincar");
+    public void Brincar(){
+      System.out.pritnln("No Puedo brincar");
+    }//end Brincar
   
-  }
+  }//end Habilidad_Motriz_Pies
     
-    public void Habilidad_Manos(){
-      System.out.println("Puedo suar mis manos!");
-    }
+  public void Habilidad_Motriz_Manos(){
+    System.out.println("Puedo suar mis manos!");
+  }//Habilidad_Motriz_Manos
       
-    public void Habilidad_NULL(){
-      System.out.println("No soy vegetal!");
-    }
-}
+  public void Habilidad_Motriz_NULL(){
+    System.out.println("No soy vegetal!");
+  }//end Habilidad_Motriz_NULL
+}//end Handicapped
+
+public static void main(String args[]){
+    Handicapped handi = new Handicapped();
+    
+    handi.Habilidad_Motriz_Pies();
+    handi.Habilidad_Motriz_Manos();
+    handi.Habilidad_Motriz_NULL();
+}//end main
