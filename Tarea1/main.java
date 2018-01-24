7f79005e838dd9a43aa71bcdf1183278ba936f70
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 12:10:33 -0800

    Create main.java

---
+++ Tarea1/main.java
@@ -0,0 +1,12 @@
+public static void main(String args[]){
+  Cocinero chef = new Cocinero();
+  Estudiante student = new Estudiante();
+  
+  System.out.println("Estas personas puede moverse bien!");
+  student.Move();
+  chef.Mover();
+  
+  Cocinero chefno = new Cocinero();
+  chefno.Habilidad_Motriz
+  Estudiante estudihambre = new Estudiante();
+}
