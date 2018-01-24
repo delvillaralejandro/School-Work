d93efe7a8ce160875135d0a68959a8a7f08e0d9e
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 08:53:23 -0800

    Update Cocinero.java

--- Tarea1/Cocinero.java
+++ Tarea1/Cocinero.java
@@ -2,4 +2,19 @@ public class Cocinero extends Persona{
   String especialidad;
   String puesto;
   
+    public void Habilidad_Pies(){
+      System.out.println("Puedo usar mis pies!");
+
+      public void Caminar(){
+       System.out.println("Puedo Caminar como Cocinero!");
+      }
+
+      public void Brincar(){
+        System.out.pritnln("Puedo brincar como Cocinero!");
+  
+  }
+    
+    public void Habilidad_Manos(){
+      System.out.println("Puedo usar mis manos!");
+    }
 }
