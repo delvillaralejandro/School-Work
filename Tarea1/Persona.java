21c2562708455bd48e094367b58d2520ac93b704
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 08:49:34 -0800

    Update Persona.java

--- Tarea1/Persona.java
+++ Tarea1/Persona.java
@@ -1,14 +1,14 @@
-class Persona{
+class Persona implements Habilidad_Motriz{
   String nombre;
   int edad;
   boolean genero;
   
-  int Caminar(){
+  /*int Caminar(){
     int posicion;
     
     posicion += 1;  
     return posicion;
-  }
+  }*/
   
   String Hablar(){
     System.out.println("Estoy hablando!");
@@ -19,7 +19,21 @@ class Persona{
     return 0;
   }
   
-  void Brincar(){}
+  public void Habilidad_Pies(){
+      System.out.println("Puedo usar mis pies!");
+
+      public void Caminar(){
+       System.out.println("Puedo Caminar!"); 
+      }
+
+      public void Brincar(){
+        System.out.pritnln("Puedo brincar");
+  
+  }
+    
+    public void Habilidad_Manos(){
+      System.out.println("Puedo suar mis manos!");
+    }
   
   
 }
