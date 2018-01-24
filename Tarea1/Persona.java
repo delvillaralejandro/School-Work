06acfc380dd696496b37e3464fdf3100369c8faf
delvillaralejandro <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 10:59:28 -0800

    Update Persona.java

--- Tarea1/Persona.java
+++ Tarea1/Persona.java
@@ -12,31 +12,28 @@ class Persona implements Habilidad_Motriz{
   
   String Hablar(){
     System.out.println("Estoy hablando!");
-    
-  }
+  }//End Hablar()
   
   void Respirar(){
     return 0;
-  }
+  }//end Respirar()
   
   public void Habilidad_Motriz_Pies(){
-      System.out.println("Puedo usar mis pies!");
-
-      public void Caminar(){
-       System.out.println("Puedo Caminar!"); 
-      }
-
-      public void Brincar(){
-        System.out.pritnln("Puedo brincar");
-  
-  }
+    System.out.println("Puedo usar mis pies!");
     
-    public void Habilidad_Motriz_Manos(){
-      System.out.println("Puedo suar mis manos!");
-    }
+    public void Caminar(){
+      System.out.println("Puedo Caminar!"); 
+    }//end Caminar()
+    public void Brincar(){
+      System.out.pritnln("Puedo brincar");
+    }//end Brincar()
+  }//End Habiliad_Motriz_Pies
   
-    public void Habilidad_Motriz_NULL(){
+  public void Habilidad_Motriz_Manos(){
+     System.out.println("Puedo suar mis manos!");
+  }//end Habilidad_Motriz_Manos()
+  
+  public void Habilidad_Motriz_NULL(){
       System.out.println("No soy Vegetal!");
-    }
-  
-}
+  }//end Habilidad_Motriz_NULL
+}//End Persona
