c93ac0e061cea75225e5ccb4e43df0fe4cd402f0
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-24 11:55:42 -0800

    Create Habilidad_Motriz_NULL.java

---
+++ Tarea1/Habilidad_Motriz_NULL.java
@@ -0,0 +1,5 @@
+public class Habilidad_Motriz_NULL implements Habilidad_Motriz{
+  public void Mover(){
+    System.out.pritnln("Yo no me puedo mover :(");
+  }//end Mover
+}//End Habilidad_Motriz_NULL
