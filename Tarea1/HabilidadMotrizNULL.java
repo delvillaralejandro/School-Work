46bb42d6bf57104184303a0111a1f0ba8778fa2f
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-29 16:41:14 -0800

    Update and rename Habilidad_Motriz_NULL.java to HabilidadMotrizNULL.java

---
+++ Tarea1/HabilidadMotrizNULL.java
@@ -0,0 +1,6 @@
+public class HabilidadMotrizNULL implements HabilidadMotriz{
+  @Override
+  public void Mover(){
+    System.out.println("Yo no me puedo mover :c \n");
+  }//end Mover
+}//End Habilidad_Motriz_NULL
--- Tarea1/Habilidad_Motriz_NULL.java
+++
@@ -1,5 +0,0 @@
-public class Habilidad_Motriz_NULL implements Habilidad_Motriz{
-  public void Mover(){
-    System.out.pritnln("Yo no me puedo mover :(");
-  }//end Mover
-}//End Habilidad_Motriz_NULL
