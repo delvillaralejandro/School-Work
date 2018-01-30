31d4df77e73cffc11daf334410bee632fcbc4db8
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-29 16:42:02 -0800

    Update and rename Habilidad_Motriz_Poderoso.java to HabilidadMotrizPoderoso.java

---
+++ Tarea1/HabilidadMotrizPoderoso.java
@@ -0,0 +1,6 @@
+public class HabilidadMotrizPoderoso implements HabilidadMotriz{
+  @Override
+  public void Mover(){
+    System.out.println("Yo puedo caminar y moverme!\n");
+  }//end Mover
+}//end Habilidad_Motriz_Poderoso
--- Tarea1/Habilidad_Motriz_Poderoso.java
+++
@@ -1,5 +0,0 @@
-public class Habilidad_Motriz_Poderoso implements Habilidad_Motriz{
-  public void Mover(){
-    System.out.println("Yo puedo caminar y moverme!");
-  }//end Mover
-}//end Habilidad_Motriz_Poderoso
