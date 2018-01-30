9d1d6893902a82867e5adc9d060b941b80f38f05
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-29 16:39:50 -0800

    Update Cocinero.java

--- Tarea1/Cocinero.java
+++ Tarea1/Cocinero.java
@@ -1,5 +1,20 @@
 public class Cocinero extends Persona{
   String especialidad;
   String puesto;
+  
+  public Cocinero(){
+    Mover(new HabilidadMotrizPoderoso());
+  }//end Cocinero
+  
+  
+  public Cocinero(HabilidadMotriz h){
+    Mover(h);
+  }//end Cocinero(HabilidadMotriz h)
+  
+  public void Movimiento(){
+    System.out.println("Soy Cocinero!");
+    hm.Mover();
+  }
+  
 }//end Cocinero class
 
