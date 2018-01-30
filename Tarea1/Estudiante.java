3f3445f5d0c925be906d7f0c9bd5932f8a7f935e
Alejandro Del Villar <35735335+delvillaralejandro@users.noreply.github.com>
2018-01-29 16:40:15 -0800

    Update Estudiante.java

--- Tarea1/Estudiante.java
+++ Tarea1/Estudiante.java
@@ -1,4 +1,18 @@
 public class Estudiante extends Persona{
   String carrera;
   int matricula;
-}//end main
+  public Estudiante(){
+    Mover(new HabilidadMotrizPoderoso());
+  }//end Estudiante
+  
+  public Estudiante(HabilidadMotriz h){
+    Mover(h);
+  }//end Estudiante(Habilidad Motiz h)
+  
+  @Override
+  public void Movimiento(){
+    System.out.println("Soy un Estudiante!");
+    hm.Mover();
+  }//end Movimiento
+  
+}//end Estudiante class
